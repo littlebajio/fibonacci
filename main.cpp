1b8333a27d7d0d455fa720557d55eb64608288d5
tulebayeva <anelek030704@gmail.com>
2023-06-07 18:10:42 +0600

    third vers

--- main.cpp
+++ main.cpp
@@ -1,16 +1,55 @@
 #include <iostream>
-int fibonacci(int number)
+using namespace std;
+int Fib(int i)
+
 {
-    if (number == 0)
-        return 0; // 
-    if (number == 1)
-        return 1;
-    return fibonacci(number-1) + fibonacci(number-2);
+int f1 = 0;
+int f2 = 1;
+int fn;
+if (i < 1) { return 0; }
+if (i == 1){ cout << "0 1\n"; }
+if (i > 1)
+{
+
+cout << "0 1 ";
+for (int j = 1; j < i;j++)
+
+{
+fn = f1 + f2;
+cout << fn << " ";
+f1 = f2;
+f2 = fn;
+
 }
+
+}
+cout << "\n\n";
+
+return 0;
+
+};
 int main()
 {
-    for (int count=0; count < 13; ++count)
-        std:: cout << fibonacci(count) << " ";
 
-    return 0;
+char choice;
+int input;
+cout << "Enter how long you wish the Fibanacci series to display: ";
+cin >> input;
+Fib(input);
+cout << "Again? Y/N: ";
+cin >> choice;
+while (choice == 'y' || choice == 'Y')
+
+{
+cout << "\nEnter how long you wish the Fibanacci series to display: ";
+cin >> input;
+Fib(input);
+cout << "Again? Y/N: ";
+cin >> choice;
+
+}
+
+system("Pause");
+return 0;
+
 }
